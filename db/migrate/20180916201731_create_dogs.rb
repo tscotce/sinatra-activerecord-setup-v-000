6e80e8c1398662a6a90b844cc4c7465b2b02d7e1
Tatyana Scotce <tscotce@gmail.com>
2018-09-16 20:20:20 +0000

    automatically backed up by learn

--- db/migrate/20180916201731_create_dogs.rb
+++ db/migrate/20180916201731_create_dogs.rb
@@ -1,15 +1,19 @@
 class CreateDogs < ActiveRecord::Migration
   def change
-  end
-  
-  def up
     create_table :dogs do |t|
       t.string :name
       t.string :breed
     end
   end
   
-  def down
-    drop_table :dogs
-  end
+  # def up
+  #   create_table :dogs do |t|
+  #     t.string :name
+  #     t.string :breed
+  #   end
+  # end
+  
+  # def down
+  #   drop_table :dogs
+  # end
 end
