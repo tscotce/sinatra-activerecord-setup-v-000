642fb60bf06a373d58e15c9068d21e3c7ce9d53e
Tatyana Scotce <tscotce@gmail.com>
2018-09-16 20:20:29 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -4,11 +4,16 @@ source "https://rubygems.org"
 gem 'sinatra'
 gem 'thin'
 gem 'require_all'
+gem 'sinatra'
+gem 'activerecord', '4.2.5'
+gem 'sinatra-activerecord'
 
 
 group :development do
 	gem 'shotgun'
 	gem 'pry'
+	gem 'sqlite3'
+	gem 'tux'
 end
     
 group :test do
---
+++ db/migrate/20180916201731_create_dogs.rb
@@ -0,0 +1,19 @@
+class CreateDogs < ActiveRecord::Migration
+  def change
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
+  end
+  
+  # def up
+  #   create_table :dogs do |t|
+  #     t.string :name
+  #     t.string :breed
+  #   end
+  # end
+  
+  # def down
+  #   drop_table :dogs
+  # end
+end
